735f01baecbe82dd59b59d042d74f2d18c255579
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-07-01 02:39:33 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt update && sudo apt full-upgrade -y
+apt install gnupg
+echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
+wget 'https://archive.kali.org/archive-key.asc'
+sudo apt-key add archive-key.asc
+apt update && sudo apt full-upgrade -y
+apt install powershell-empire beef-xss apache2 -y 
+apt update; apt install git -y; git clone https://github.com/mhaskar/Octopus/ /opt/Octopus; cd /opt/Octopus/; apt install python3-pip -y ; export LC_ALL=C ;  pip3 install -r requirements.txt;apt install mono-devel -y
