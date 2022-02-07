04576f50a0e5a6c464fc19e011b0bafa82747a2a
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2022-02-07 23:47:38 +0200

    fix

--- install.sh
+++ install.sh
@@ -3,14 +3,9 @@ apt install git curl gnupg -y
 echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
 wget 'https://archive.kali.org/archive-key.asc'
 sudo apt-key add archive-key.asc
-sudo apt update;sudo apt upgrade -y ;sudo apt install powershell-empire
+sudo apt update;sudo apt upgrade -y ;sudo apt install python3-pip powershell-empire
 apt install metasploit-framework -y
-cd /opt
-git clone https://github.com/zerosum0x0/koadic.git
-cd koadic
-pip3 install -r requirements.txt
-echo "cd /opt/koadic;./koadic" >> /usr/bin/koadic
-chmod +x /usr/bin/koadic
+apt install koadic
 apt install nasm -y
 apt install mingw-w64 mono-devel -y
 cd /opt
